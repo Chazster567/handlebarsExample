6fe10fc5b541243f4a30e166190f43b3daae3508
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2020-03-31 20:35:10 +0100

    Updated Form to receive data and display messages.

--- server.js
+++ server.js
@@ -1,19 +1,55 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Contact = require('./models/Contact');
 
 app.set('view engine', 'hbs');
+
 app.engine('hbs', handlebars({
     layoutDir: __dirname + '/views/layouts',
     extname: 'hbs'
 }))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.render('index', { layout: 'main' });
+    Contact.find({}).lean()
+    .exec((err, contacts) => {
+        if(contacts.length){
+            res.render('index', { layout: 'main', contacts: contacts, contactsExist: true });
+        } else{
+            res.render('index', { layout: 'main', contacts: contacts, contactsExist: false });
+        }
+    })
+});
+
+app.post('/addContact', (req, res) =>{
+    const { name, email, number } = req.body;
+    var contact = new Contact({
+        name,
+        email,
+        number
+    });
+    contact.save();
+    res.redirect('/');
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { layout: 'main' });
+})
+
+mongoose.connect('mongodb://localhost:27017/handlebars', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(() => {
+    console.log('Connected to the DB');
+})
+.catch((err) => {
+    console.log('Not connected to the DB : ' + err);
 })
 
 //listening for requests on port 3000
